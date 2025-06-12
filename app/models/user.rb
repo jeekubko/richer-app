dd540e8dc968726bc44b0ad4f633c4b02a70286d
Jakub Vaclavik <jakub.vaclavik@muziker.com>
2025-06-12 10:06:01 +0200

    Implement devise

---
+++ app/models/user.rb
@@ -0,0 +1,6 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable, :validatable
+  validates :given_name, :family_name, presence: true
+end
