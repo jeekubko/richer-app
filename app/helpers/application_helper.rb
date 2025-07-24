28a2bf9fe5f0ea79b146df17708f385b64e67fe0
jeekubko <jakubvaclavik1@gmail.com>
2025-07-24 14:27:46 +0200

    Add transactions (#3)
    * Add transactions
    
    * Add styling
    
    * Add validations
    
    ---------
    
    Co-authored-by: Jakub Vaclavik <jakub.vaclavik@muziker.com>

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,16 @@
 module ApplicationHelper
+  def active_path(path)
+    'active' if request.path == path
+  end
+
+  def active_controller(controller)
+    'active' if controller_name == controller
+  end
+
+  def active_for(path: nil, controller: nil)
+    if path && request.path == path ||
+         controller && controller_name == controller
+      'active'
+    end
+  end
 end
