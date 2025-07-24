3b174273e16ac8aa2833a8d99534619687ebf150
Jakub Vaclavik <jakub.vaclavik@muziker.com>
2025-07-24 17:58:41 +0200

    Add user id to transactions

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  has_many :transactions, dependent: :delete_all
+
   devise :database_authenticatable, :registerable, :validatable
   validates :given_name, :family_name, presence: true
 end
